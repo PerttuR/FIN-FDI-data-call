db7052deaa8ce464770a7d966d401d7d0543d19f
= <=>
2018-06-19 11:13:30 +0300

    next table to github

---
+++ table_c.R
@@ -0,0 +1,14 @@
+#-------------------------------------------------------------------------------
+#
+# Script to process FIN- commercial data for STECF FDI data call - TABLE C
+#
+# Coded: Perttu Rantanen, Mira Sustar, Petri Sarvamaa
+#
+# Date: JUN-2018
+#
+# Client: LUKE EU-DCF project
+#-------------------------------------------------------------------------------
+
+#--------------------READ ME----------------------------------------------------
+# The following script is for preparing FDI data tables from Table A from statistical DEP (Pirkko)
+#-------------------------------------------------------------------------------
