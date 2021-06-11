8e61b0cb8e904f2c5927722a4c39d565665e942e
Perttu Rantanen <perttu.rantanen@luke.fi>
2021-06-11 09:53:12 +0300

    code walkthrough and documentation and some minor changes. output to XLSX

--- table_b.R
+++ table_b.R
@@ -48,7 +48,7 @@ path_out <- paste0(getwd(), "/results/2021") # folder where the output is saved
 #                       1. set TABLE B columns                       
 #-------------------------------------------------------------------------------
 
-#setwd(path_rproject)
+setwd(path_rproject)
 
 source("db.R")
 
@@ -56,29 +56,36 @@ sampling_result <- read.dbTable("suomu","sampling_result")
 sampling_source <- read.dbTable("suomu","sampling_source")
 sampling_source_weight <- read.dbTable("suomu","sampling_source_weight")
 
+#fetch lottery status levels
 sampling_result_status_map <- data.frame(key=seq(from=0,to=7),value=c("assigned","inactive","rejected","sampled","out of area","no_contact_details","no_answer","observer_declined"))
+#join status to sampling PSU source data
 sampling_result_source <- sampling_result %>% left_join(sampling_source, by=c("sample_source_fk"="id")) %>%
   left_join(sampling_result_status_map, by=c("status" = "key")) %>% mutate(status = value)
-
+#set reference year PSU counts to sampling year
 sampling_result_source_fixed_year <- sampling_result_source %>% mutate(year = year+1)
-
 
 
 species <- read.dbTable("suomu","species")
 gear <- read.dbTable("suomu","gear")
 
+#get follow up tables
 seurantataulukot <- read.dbTable("suomu","tracking_metier_name")
 tracking_metier <- read.dbTable("suomu","tracking_metier")
 tracking_species <- read.dbTable("suomu", "tracking_species");
 metier <- read.dbTable("suomu","metier")
 
+#get trip and haul data
 trip <- read.dbTable("suomu", "trip")
 haul <- read.dbTable("suomu", "haul")
 
+#lottery machine was operational since 2018 Q4
 trip_selected <- trip %>% filter(!is.na(target_species_fk) & year >= 2018)
 trip_haul <- trip_selected %>% left_join(haul, by=c("id" = "trip_fk"))
 
+#join trip haul and metier to form SAMPLING_FRAME column:
+#-----
 trip_haul_metier <- trip_haul %>% left_join(metier, by=c("metier_fk" = "id"))
+
 species_gear_code <- trip_haul_metier %>% select(target_species_fk, level5)
 
 species_gear_code_tally <- species_gear_code %>% group_by(target_species_fk, level5) %>% tally()
@@ -92,64 +99,37 @@ species_gear_code_tally3 <- species_gear_code_tally2 %>% mutate(frame = case_whe
 
 species_metier_map <- species_gear_code_tally3
 
-#sampling_selection <- sampling_source %>% left_join(sampling_result, by = c("id" = "sample_source_fk"))
-#sampling_selection2 <- sampling_selection %>% left_join(gear, by = c("kake_gear_id" = "id"))
-#
-#
-#
-#metier_vessel <- metier %>% filter(gear_code == 'OTM' | gear_code == 'PTM')
-#sampling_selection3 <- sampling_selection2 %>% left_join(metier_vessel, by = c("fishframe" = "gear_code"))
-#
-#
-##design_metiers <- seurantataulukot %>% filter(sampling_target == TRUE)
-##design_metiers2 <- design_metiers %>% left_join(tracking_metier, by = c("id" = "tracking_metier_name_fk"))
-##design_metiers3 <- design_metiers2 %>% left_join(metier, by = c("metier_fk" = "id"))
-#
-#
-#lottery_raw$real_year <- lottery_raw$year + 1
-#lottery_raw2 <- lottery_raw %>% left_join(lottery_results, by = c("id" = "sample_source_fk"))
-#
-#lottery_raw3 <- lottery_raw2 %>% group_by(real_year, quarter, area, tracking_metier_name_fk) %>% summarise(REFUSAL = length(as.numeric(as.character(status))))
-#
-#
-#tracking_metier2 <- tracking_metier %>% left_join(design_metiers)
-#
-#
-#design_metiers$metier_fk <-  design_metiers$id
-#design_metiers <- design_metiers %>% select(metier_fk)
-#
-#table_b <- table_b %>% select(COUNTRY, YEAR, SAMPLE_FRAME, REFUSAL_RATE, COVERAGE_RATE, NONRESPONSE_RATE, VESSELS_FLEET, TRIPS_FLEET, TRIPS_SAMPLED_ONBOARD, UNIQUE_VESSELS_SAMPLED, UNIQUE_VESSELS_CONTACTED, NOT_AVAILABLE, NO_CONTACT_DETAILS, NO_ANSWER, OBSERVER_DECLINED, INDUSTRY_DECLINED, SUCCESS_RATE, TOT_SELECTIONS)
-
-#-------------------------------------------------------------------------------
+#-----
 
 
 #-------------------------------------------------------------------------------
 #                       2. set TABLE B columns                      
 #-------------------------------------------------------------------------------
 
-#table_b$COUNTRY <-"FIN"
-#table_b$YEAR <-"2019"
-#table_b$SAMPLE_FRAME <- "B5(OTM_SPF&PTM_SPF) Q2SD30"
 
 #-------------------------------------------------------------------------------
-# choose upper (WP&AR) hierarcy gears/metiers only (DEL national sub metiers)
+# tekstiä
 
-wp_gears <- c("OTM", "PTM", "FYK", "GNS")
-metiers <- filter(metier, gear_code %in% wp_gears) 
+#wp_gears <- c("OTM", "PTM", "FYK", "GNS")
+#metiers <- filter(metier, gear_code %in% wp_gears) 
+
+#FDI data call years:
 years <- seq(from=2014, to=2020)
 
 sampling_result_source_filtered <- sampling_result_source_fixed_year %>% filter(year %in% years)
 
+#manipulate sampling result table data:
 sampling_result_frame <- sampling_result_source_filtered %>%
   left_join(tracking_species, by="tracking_metier_name_fk") %>%
   left_join(species_metier_map, by=c("species_fk"= "target_species_fk"))
 
+#Set data call columns
 sampling_result_frame_quarter_sd <- sampling_result_frame %>% mutate(frame = paste0(frame, ' Q', quarter, ' SD', area))
 sampling_result_frame_quarter_sd$year <- as.factor(sampling_result_frame_quarter_sd$year)
 sampling_result_frame_quarter_sd$frame <- as.factor(sampling_result_frame_quarter_sd$frame)
-
 sampling_result_frame_quarter_sd$COUNTRY <- "FIN";
 
+#form table B:
 table_b <- sampling_result_frame_quarter_sd %>% select(COUNTRY, YEAR=year, SAMPLE_FRAME=frame) %>% distinct() %>%
   arrange(YEAR, SAMPLE_FRAME)
 
@@ -168,55 +148,22 @@ table_b$OBSERVER_DECLINED <- 0
 table_b$INDUSTRY_DECLINED <- 0
 table_b$TOT_SELECTIONS <- 0
 
+#Aggragate table B to sampling frame level:
 sampling_result_grouped <- sampling_result_frame_quarter_sd %>% mutate(year_frame = paste0(year, frame))
 sampling_result_grouped$year_frame <- as.factor(sampling_result_grouped$year_frame)
 sampling_result_grouped <- sampling_result_grouped %>% group_by(year_frame, .drop = FALSE) %>% arrange(year_frame)
+
 #TODO: call count
+
+#count Refusals
 tally_rejection <- sampling_result_grouped %>% filter(status == "rejected") %>% tally()
+
+#count contacts
 tally_all <- sampling_result_grouped %>% summarise(sum=sum(if_else(call_count == 0,1,as.double(call_count))))
 
 table_b$TOT_SELECTIONS <- tally_all$sum
 
-write.csv(table_b, "FIN_TABLE_B_REFUSAL_RATE.csv", row.names = F)
-
-
-
-#select project and years
-samples <- samples %>% filter(project == "EU-tike(CS, kaupalliset n?ytteet)" & year %in% years)
-#merge national metiers to DCF
-samples <- samples %>% mutate(level6 = replace(level6, level6 == 'FPN_FWS_>0_0_0', 'FYK_FWS_>0_0_0'), level6 = replace(level6, level6 == 'FPN_SPF_>0_0_0', 'FYK_SPF_>0_0_0'))
-#summarise samples
-samples <- samples %>% group_by(year, level6) %>% summarise(trip_count = sum(trip_count))
-
-
-
-# import data from salmon samples
-setwd(path_tablea)
-salmon <- read.csv("stecf.csv", sep = ";", header = T)
-salmon2 <- salmon %>% group_by(YEAR, METIER) %>% summarise(trip_count = n_distinct(DB_TRIP_ID))
-salmon2 <- salmon2 %>% rename(year = YEAR, level6=METIER)
-salmon2 <- salmon2 %>% filter(level6 == "FYK_ANA_0_0_0")
-salmon2$level6 <- 'FYK_ANA_>0_0_0'
-
-samples <- bind_rows(samples, salmon2)
-
-table_b <- left_join(table_b,samples,  by = c("year", "level6"))
-
-table_b <- table_b %>% rename(YEAR = year, SAMPLE_FRAME = level6, SUCCESFUL_SAMPLE = trip_count)
-table_b <- table_b[order(table_b$YEAR),]
-
-table_b <- table_b[-which(is.na(table_b$SUCCESFUL_SAMPLE)),]
-
-
-setwd(path_tablea)
-trips <- read.xlsx("Tripit ja alukset 2015-2018.xlsx", sheet="rk_pvk", header = T)
-?read.xlsx
-
-# set working directory to save table B and table
 setwd(path_out)
-write.csv(table_b, "FIN_TABLE_B_REFUSAL_RATE.csv", row.names = F)
-#install.packages('xlsx')
-library(xlsx)
-write.xlsx(table_b, "FIN_TABLE_B_REFUSAL_RATE.xlsx", sheetName="Sheet1", col.names=TRUE, row.names=FALSE,)
+write.xlsx(table_b, "FIN_TABLE_B_REFUSAL_RATE.xlsx", sheetName = "TABLE_B", col.names=TRUE, row.names=FALSE)
 
 
