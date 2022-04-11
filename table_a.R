7c8c23cbacbc439258174cbba796b27a5019a724
Perttu Rantanen <perttu.rantanen@luke.fi>
2022-04-11 10:03:55 +0300

    Common paths ander working directory to resource and output files

--- table_a.R
+++ table_a.R
@@ -35,20 +35,19 @@ path_tablea <- "C:/2018/FDI/work/data/orig/" # folder where TABLE A is (FIN_TABL
 path_rproject <- "C:/2018/FDI/work/prog/FIN-FDI-data-call/" # folder where the r project is (and the source file db.R!)
 path_out <- "C:/2018/FDI/work/data/der/" # folder where the output is saved
 
-# Perttu:
-path_tablea <- "C:/perttu/eu-tike/STECF/FIN-FDI-data-call/orig" # folder where TABLE A is (TABLE_A_CATCH.csv)
-path_rproject <- "C:/perttu/eu-tike/STECF/FIN-FDI-data-call" # folder where the r project is (and the source file db.R!)
-path_out <- "C:/perttu/eu-tike/STECF/FIN-FDI-data-call/results/2021" # folder where the output is saved
+# Common paths & 2022 folder:
+path_tablea <- paste0(getwd(), .Platform$file.sep, "orig/") # folder where TABLE A is (FIN_TABLE_A_CATCH.csv)
+path_rproject <- getwd() # folder where the r project is (and the source file db.R!)
+# folder where the output is saved
+path_out <- paste0(getwd(), .Platform$file.sep,"results", .Platform$file.sep,"2022")
 
 #-------------------------------------------------------------------------------
 #                       1. import TABLE A and save it as .xlsx                       
 #-------------------------------------------------------------------------------
-setwd(path_tablea)
 
 # import table A
-table_A <- read.csv2("A_table_2014_2020.csv", sep = "," , na.strings = "")
+table_A <- read.csv2(paste0(path_tablea,.Platform$file.sep,"A_table_2014_2020.csv"), sep = "," , na.strings = "")
 
-setwd(path_out)
 #select order of columns
 table_A <- table_A %>% select(COUNTRY,	YEAR, QUARTER, VESSEL_LENGTH,	FISHING_TECH,	GEAR_TYPE,	TARGET_ASSEMBLAGE,	MESH_SIZE_RANGE,	METIER,	DOMAIN_DISCARDS,	DOMAIN_LANDINGS,	SUPRA_REGION,	SUB_REGION,	EEZ_INDICATOR,	GEO_INDICATOR,	NEP_SUB_REGION,	SPECON_TECH,	DEEP,	SPECIES,	TOTWGHTLANDG,	TOTVALLANDG,	DISCARDS,	CONFIDENTIAL)
 
@@ -74,6 +73,6 @@ table_A$QUARTER <- as.character(table_A$QUARTER)
 #METIER set NK when TARGET_ASSEMBLAGE == NK
 #table_A <- table_A %>% mutate(METIER = replace(as.character(METIER), which(as.character(METIER)=="MIS_MIS_0_0_0" & as.character(TARGET_ASSEMBLAGE) == "NK") , "NK"))
 
-write.xlsx(table_A, "TABLE_A_CATCH.xlsx", sheetName = "TABLE_A", col.names = TRUE, row.names = FALSE)
+write.xlsx(table_A,paste0(path_out,.Platform$file.sep,"TABLE_A_CATCH.xlsx"), sheetName = "TABLE_A", col.names = TRUE, row.names = FALSE)
 
 #-------------------------------------------------------------------------------
