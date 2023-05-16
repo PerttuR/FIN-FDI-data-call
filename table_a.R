29a509af3e9b93e8efc9b7b5708021b3acbc5f37
Sykkö Antti (LUKE) <antti.sykko@luke.fi>
2023-05-16 20:15:10 +0300

    tested import & script preparation

--- table_a.R
+++ table_a.R
@@ -38,30 +38,43 @@ path_tablea <- paste0(getwd(), .Platform$file.sep, "orig/") # folder where TABLE
 path_rproject <- getwd() # folder where the r project is (and the source file db.R!)
 # folder where the output is saved
 path_out <- paste0(getwd(), .Platform$file.sep,"results", .Platform$file.sep,"2022")
+#-------------------------------------------------------------------------------
 
 #-------------------------------------------------------------------------------
-#                       1. import TABLE A and save it as .xlsx                       
+#                       1. Import table A                      
 #-------------------------------------------------------------------------------
 
 # import table A
-table_A <- read.csv2(paste0(path_tablea,.Platform$file.sep,"A_table_2013_2021.csv"), sep = "," , na.strings = "")
+table_A <- read.csv2(paste0(path_tablea,.Platform$file.sep,"A_table_2013_2022.csv"), sep = "," , na.strings = "")
 
 #select order of columns
 table_A <- table_A %>% select(COUNTRY,	YEAR, QUARTER, VESSEL_LENGTH,	FISHING_TECH,	GEAR_TYPE,	TARGET_ASSEMBLAGE,	MESH_SIZE_RANGE,	METIER,	DOMAIN_DISCARDS,	DOMAIN_LANDINGS,	SUPRA_REGION,	SUB_REGION,	EEZ_INDICATOR,	GEO_INDICATOR,	NEP_SUB_REGION,	SPECON_TECH,	DEEP,	SPECIES,	TOTWGHTLANDG,	TOTVALLANDG,	DISCARDS,	CONFIDENTIAL)
+#-------------------------------------------------------------------------------
+
+
+#-------------------------------------------------------------------------------
+#                       2. Modify table A                      
+#-------------------------------------------------------------------------------
 
 # rename columns to UPPER and save as .xlsx
 colnames(table_A)    <- c("COUNTRY",	"YEAR",	"QUARTER",	"VESSEL_LENGTH",	"FISHING_TECH",	"GEAR_TYPE",	"TARGET_ASSEMBLAGE",	"MESH_SIZE_RANGE",	"METIER",	"DOMAIN_DISCARDS",	"DOMAIN_LANDINGS",	"SUPRA_REGION",	"SUB_REGION",	"EEZ_INDICATOR",	"GEO_INDICATOR",	"NEP_SUB_REGION",	"SPECON_TECH",	"DEEP",	"SPECIES",	"TOTWGHTLANDG",	"TOTVALLANDG",	"DISCARDS",	"CONFIDENTIAL")
 
 
 
-# rounding the number to three digits precision
+# ... rounding the number to three digits precision
 
 table_A$TOTWGHTLANDG <- round(as.numeric(as.character(table_A$TOTWGHTLANDG)), digits = 3)
 table_A$TOTVALLANDG <- round(as.numeric(as.character(table_A$TOTVALLANDG)), digits = 3)
 table_A$DISCARDS <- round(as.numeric(as.character(table_A$DISCARDS)), digits = 3)
 
-#quartes to string
+# ... quartes to string
 table_A$QUARTER <- as.character(table_A$QUARTER)
+
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+#                       3. Validate table A                      
+#-------------------------------------------------------------------------------
 
 #FDI database did not allow "=" in DOMAINS.. now fixed
 #library(stringr)
@@ -71,6 +84,13 @@ table_A$QUARTER <- as.character(table_A$QUARTER)
 #METIER set NK when TARGET_ASSEMBLAGE == NK
 #table_A <- table_A %>% mutate(METIER = replace(as.character(METIER), which(as.character(METIER)=="MIS_MIS_0_0_0" & as.character(TARGET_ASSEMBLAGE) == "NK") , "NK"))
 
-write.xlsx(table_A,paste0(path_out,.Platform$file.sep,"TABLE_A_CATCH.xlsx"), sheetName = "TABLE_A", col.names = TRUE, row.names = FALSE)
+
+#-------------------------------------------------------------------------------
+#                       4. Write table A                      
+#-------------------------------------------------------------------------------
+
+write.xlsx(table_A,paste0(path_out,.Platform$file.sep,"TABLE_A_CATCH.xlsx"), 
+           sheetName = "TABLE_A", col.names = TRUE, row.names = FALSE)
+
 
 #-------------------------------------------------------------------------------
