a84b6ecd86ba8cf99f7b648282538980924703fc
Sykkö Antti (LUKE) <antti.sykko@luke.fi>
2023-05-29 12:44:03 +0300

    fixed metier and mesh size ranges

--- table_h.R
+++ table_h.R
@@ -2,11 +2,12 @@
 #
 # Script to process FIN- commercial data for STECF FDI data call - TABLE H
 #
-# Coded: Perttu Rantanen, Mira Sustar, Petri Sarvamaa, Anna-Kaisa Ylitalo
+# Coded: Perttu Rantanen, Mira Sustar, Petri Sarvamaa, Anna-Kaisa Ylitalo, Antti Sykkö
 #
 # Date: JUN-2018
 # Updated: JUN 2021 by Perttu
 #          JUN-2022 by Anna-Kaisa and Perttu
+#          MAY-2023 by Antti
 #
 # Client: LUKE EU-DCF project
 #-------------------------------------------------------------------------------
@@ -34,7 +35,7 @@ library(xlsx)
 # Common paths & 2022 folder:
 path_tableh <- paste0(getwd(), .Platform$file.sep, "orig/") # folder where TABLE H is
 # Output folder
-path_out <- paste0(getwd(), .Platform$file.sep,"results", .Platform$file.sep,"2022")
+path_out <- paste0(getwd(), .Platform$file.sep,"results", .Platform$file.sep,"2023")
 
 
 #-------------------------------------------------------------------------------
@@ -42,7 +43,6 @@ path_out <- paste0(getwd(), .Platform$file.sep,"results", .Platform$file.sep,"20
 #-------------------------------------------------------------------------------
 
 # import table H
-
 table_H <- read.csv2(paste0(path_tableh,"H_table_2013_2022.csv"), sep = "," ,na.strings="")
 
 
@@ -68,7 +68,20 @@ table_H <- left_join(table_H, midpoints,copy = TRUE)
 table_H <- table_H %>% rename(RECTANGLE_LAT = SI_LATI, RECTANGLE_LON = SI_LONG)
 
 # .. add empty col for new metier
-table_H$METIER_7 <- NA
+table_H$METIER_7 <- 'NA'
+
+
+
+# .. Invalid code: GNS_SPF_16-109_0_0
+table_H$METIER <- ifelse(table_H$METIER == "GNS_SPF_16-109_0_0", "GNS_SPF_16-31_0_0", table_H$METIER)
+# .. Invalid code: OTM_DEF_>=105_1_120
+table_H$METIER <- ifelse(table_H$METIER == "OTM_DEF_>=105_1_120", "OTM_DEF_105-115_1_120", table_H$METIER)
+# .. Invalid code: OTM_SPF_16-104_0_0
+table_H$METIER <- ifelse(table_H$METIER == "OTM_SPF_16-104_0_0", "OTM_SPF_16-31_0_0", table_H$METIER)
+# .. Invalid code: PTM_SPF_16-104_0_0
+table_H$METIER <- ifelse(table_H$METIER == "PTM_SPF_16-104_0_0", "PTM_SPF_16-31_0_0", table_H$METIER)
+# .. Invalid code: OTB_DEF_>=105_1_120
+table_H$METIER <- ifelse(table_H$METIER == "OTB_DEF_>=105_1_120", "OTB_DEF_115-120_0_0", table_H$METIER)
 
 
 
@@ -100,15 +113,37 @@ table_H$C_SQUARE <- "NA"
 # ... convert quarters to char
 table_H$QUARTER <- as.character(table_H$QUARTER)
 
+# ... changes in colnames in 2023 
+table_H$LATITUDE <- table_H$RECTANGLE_LAT
+table_H$LONGITUDE <- table_H$RECTANGLE_LON
+
+
 # ... select & order cols 
-table_H <- table_H %>% select(COUNTRY, YEAR, QUARTER, VESSEL_LENGTH, FISHING_TECH, GEAR_TYPE, 
-                              TARGET_ASSEMBLAGE, MESH_SIZE_RANGE, METIER, METIER_7, SUPRA_REGION, SUB_REGION, 
-                              EEZ_INDICATOR, GEO_INDICATOR, SPECON_TECH, DEEP, RECTANGLE_TYPE, RECTANGLE_LAT, 
-                              RECTANGLE_LON, C_SQUARE, SPECIES, TOTWGHTLANDG, TOTVALLANDG, CONFIDENTIAL)
+table_H <- table_H %>% select(COUNTRY,	YEAR,	QUARTER,	VESSEL_LENGTH,	FISHING_TECH,	GEAR_TYPE,	TARGET_ASSEMBLAGE,
+                                MESH_SIZE_RANGE,	METIER,	METIER_7,	SUPRA_REGION,	SUB_REGION,	EEZ_INDICATOR,	GEO_INDICATOR,
+                                SPECON_TECH,	DEEP,	RECTANGLE_TYPE,	LATITUDE,	LONGITUDE,	C_SQUARE,	SPECIES,	TOTWGHTLANDG,	
+                                TOTVALLANDG,	CONFIDENTIAL)
+
+
+# NOTE 2023: a missing value in MESH_SIZE_RANGE is not anymore allowed if gear is GNS 
+#            The problem -> NA values are refering to trap code 25 = Net, unknown
+
+
+# .. modify 
+table_H$MESH_SIZE_RANGE <- ifelse(is.na(table_H$MESH_SIZE_RANGE) & table_H$METIER == "GNS_ANA_>=157_0_0", 
+                                  '157DXX', table_H$MESH_SIZE_RANGE)
+
+
+table_H$MESH_SIZE_RANGE <- ifelse(is.na(table_H$MESH_SIZE_RANGE) & table_H$METIER == "GNS_FWS_>0_0_0", 
+                                  '16D32', table_H$MESH_SIZE_RANGE)
+
+table(table_H$MESH_SIZE_RANGE, useNA = 'always')
+
 
 # save table H
-write.xlsx(table_H, paste0(path_out,.Platform$file.sep,"TABLE_H_LANDINGS_BY_RECTANGLE.xlsx"), sheetName = "TABLE_H", col.names = TRUE, row.names = FALSE)
+#write.xlsx(table_H, paste0(path_out,.Platform$file.sep,"FIN_TABLE_H_LANDINGS_BY_RECTANGLE.xlsx"), sheetName = "TABLE_H", col.names = TRUE, row.names = FALSE)
 
 ## AKY: the line above did not work for me (some java related memory issue)
-#library(openxlsx)
-#openxlsx::write.xlsx(table_H, paste0(path_out,.Platform$file.sep,"TABLE_H_LANDINGS_BY_RECTANGLE.xlsx"), sheetName = "TABLE_H", colNames = TRUE, rowNames = FALSE)
+## Antti: As for me -> we could fix this later 
+library(openxlsx)
+openxlsx::write.xlsx(table_H, paste0(path_out,.Platform$file.sep,"FIN_TABLE_H_LANDINGS_BY_RECTANGLE.xlsx"), sheetName = "TABLE_H", colNames = TRUE, rowNames = FALSE)
