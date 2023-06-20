bc3c809e236c3732fe8c8a443df7bf3bdae96d23
Rantanen Perttu (LUKE) <perttu.rantanen@luke.fi>
2023-06-20 15:36:15 +0300

    Table F 2023 uplaoded

--- table_f.R
+++ table_f.R
@@ -7,7 +7,7 @@
 # Date: JUN-2018
 # Updated: JUN 2021 by Perttu
 # Updated: JUN 2022 by Perttu
-# Updated: MAY 2023 by Perttu
+# Updated: JUN 2023 by Perttu
 #
 # Client: LUKE EU-DCF project
 #-------------------------------------------------------------------------------
@@ -47,7 +47,7 @@ path_out <- paste0(getwd(), .Platform$file.sep,"results", .Platform$file.sep,"20
 #-------------------------------------------------------------------------------
 
 # import table A
-table_A <- read.csv2(paste0(path_tablea,.Platform$file.sep,"A_table_2013_2021.csv"), sep = "," , na.strings = "")
+table_A <- read.csv2(paste0(path_tablea,.Platform$file.sep,"A_table_2013_2022.csv"), sep = "," , na.strings = "")
 #select order of columns
 table_A <- table_A %>% select(COUNTRY,	YEAR, QUARTER, VESSEL_LENGTH,	FISHING_TECH,	GEAR_TYPE,	TARGET_ASSEMBLAGE,	MESH_SIZE_RANGE,	METIER,	DOMAIN_DISCARDS,	DOMAIN_LANDINGS,	SUPRA_REGION,	SUB_REGION,	EEZ_INDICATOR,	GEO_INDICATOR,	NEP_SUB_REGION,	SPECON_TECH,	DEEP,	SPECIES,	TOTWGHTLANDG,	TOTVALLANDG,	DISCARDS,	CONFIDENTIAL)
 
@@ -87,7 +87,7 @@ lengthdata <- read.dbTable("suomu","report_lengthclassrecords")
 #-------------------------------------------------------------------------------
 # choose commercial LANDING samples only, from years 2013-2022
 
-landing <- filter(lengthdata, saalisluokka == "LANDING", projekti == "EU-tike(CS, kaupalliset näytteet)", vuosi >= 2013 & vuosi <= 2023)
+landing <- filter(lengthdata, saalisluokka == "LANDING", projekti == "EU-tike(CS, kaupalliset näytteet)", vuosi >= 2013 & vuosi <= 2022)
 
 #-------------------------------------------------------------------------------
 
@@ -132,7 +132,124 @@ landing2 <- landing %>% select(vuosi, domain_landings, nayteno, pituusluokka, pi
 # and save it to workflow orig folder
 # import data from salmon samples
 
-ana <- read.csv(paste0(path_salmon, "anadromous_samples_2013_2021.csv"), sep = ";", header = T, stringsAsFactors=FALSE)
+ana1 <- read.csv(paste0(path_salmon, "anadromous_samples_2013_2021.csv"), sep = ";", header = T, stringsAsFactors=FALSE)
+
+#--------------------------------------------------------------------------------------------
+#       3.1 Functions to use Mongo Individual data                      
+#--------------------------------------------------------------------------------------------
+
+
+
+get.rectangle <- function(individual) {
+  rectangle <- ices_rectangle[ices_rectangle$rktl_name ==first(individual$ruutu),]
+  if(nrow(rectangle) != 1) {
+    stop("Missing rectangle")
+  }
+  return(rectangle)
+}
+
+get.stock <- function(batch, rectangle) {
+  if(batch$aphia_id == 127186) {
+    if(rectangle$rdb_area_code == "27.3.d.32") {
+      return("sal-32")
+    } else {
+      return("sal-2431")
+    }
+  }
+  return("")
+}
+
+get.age <- function(individual) {
+  mevu <- as.integer(individual$me_vu)
+  povu <- as.integer(individual$po_vu)
+  povu <- ifelse(is.na(povu), 2, povu)
+  return(ifelse(is.na(mevu), NA, mevu + povu))
+}
+
+
+#--------------------------------------------------------------------------------------------
+#       3.2 Merge ANA1 (Oracle ana-samples 2013-2021) and ANA2 (MOngo 2022 onwards anadromous samples)                    
+#--------------------------------------------------------------------------------------------
+
+source("mongo.R")
+
+ices_rectangle <- read.csv2("ices_rectangle.csv", sep=",", dec=".")
+
+batch <- read.mongoCollectionToDataframe("batch")
+individual <- read.mongoCollectionToDataframe("individual")
+species <- read.mongoCollectionToDataframe("species")
+species$laji <- as.character(species$laji)
+batch <- batch %>% left_join(species, by = c("species_id" = "laji"))
+project <- read.mongoCollectionToDataframe("project")
+
+# filter only landed salmon and sea trout samples
+individual$pitcm <-  as.integer(individual$pitcm)
+# select landing salmons and sea trouts based on length
+individual <- individual  %>% filter(pitcm >= 60) 
+
+
+# Resolve trip names
+
+batch$trip_id <- paste(batch$year, batch$lajikv1, batch$batch_number, sep="_")
+
+#join batch to individual
+
+individual2 <- individual %>% left_join(batch, by = c("sample_id" = "batch_id"))
+
+#join project to individual
+
+individual3 <- individual2 %>% left_join(project, by = c("project_id" = "project_id"))
+
+#filter empty lenths
+#filter only commercial samples
+#form lengthclasses
+individual4 <- individual3 %>%
+  filter(!is.na(as.numeric(pitcm))) %>%
+  filter(number==0) %>% #filter only commercial samples included 
+  mutate(length = as.numeric(pitcm) * 10) %>%
+  mutate(lengthclass = length - length %% 10) %>%
+  arrange(lengthclass, length)
+
+#renaming
+individual4$PITUUS <- individual4$length
+individual4$DB_TRIP_ID <- individual4$trip_id
+individual4$YEAR <- individual4$year
+individual4$PYYDYSKOODI <- individual4$pyydys
+
+#Age to a single number from me_vu and po_vu variables
+individual4$IKA <- get.age(individual4)
+
+#weight in grams
+individual4$PAINO_GRAMMOINA <- individual4$pakg * 1000
+
+#calculate month
+individual4$MONTH <- format(as.Date(individual4$pvm, format="%Y-%m-%d"),"%m")
+
+#add year Quarters
+Q1 <- c("01","02","03")
+Q2 <- c("04","05","06")
+Q3 <- c("07","08","09")
+Q4 <- c("10","11","12")
+individual4$QUARTER [individual4$MONTH %in% Q1]<-1
+individual4$QUARTER [individual4$MONTH %in% Q3]<-3
+individual4$QUARTER [individual4$MONTH %in% Q4]<-4
+individual4$QUARTER [individual4$MONTH %in% Q2]<-2
+
+individual4$MONTH <- as.numeric(individual4$MONTH)
+individual4$PYYDYSKOODI <- as.numeric(individual4$PYYDYSKOODI)
+
+#rename SD, add metier, rename species code
+individual4$ICES_OA <- as.numeric(individual4$osa_al)
+individual4$METIER <- "FYK_ANA_>0_0_0"
+individual4$FAO <- individual4$lajikv1
+
+# TO DO select correct variables from MONGO data as ana2
+
+ana2 <- individual4
+#cast Oracle DB_TRIP_ID to character
+ana1$DB_TRIP_ID <- as.character(ana1$DB_TRIP_ID)
+
+ana <- dplyr::bind_rows(ana1, ana2)
 
 #rename metier to correct
 #DEL: salmon <- salmon %>% mutate(METIER=replace(METIER, METIER=="FYK_ANA_0_0_0", "FYK_ANA_>0_0_0")) %>% as.data.frame()
@@ -212,6 +329,9 @@ landing6 <- landing5 %>% select(country, vuosi, domain_landings, nep_sub_region,
 #                       5. Merge SAMPLED DATA with TABLE A                       
 #-------------------------------------------------------------------------------
 
+# 2022 data has NaN values -> delete
+table_A_sum <- table_A_sum
+
 # merge landing catch data with TABLE A
 table_f_pre <- merge(landing6, table_A_sum, by = c("country", "year", "domain_landings"), all.x = T)
 
@@ -226,9 +346,12 @@ length(missing_domains2$domain_landings)
 table_f_pre2 <- filter(table_f_pre, !is.na(totwghtlandg))
 suomu_deleted_for_no_catch <- filter(table_f_pre, is.na(totwghtlandg))
 
+table_f_pre3 <- na.omit(table_f_pre2)
+
+
 # arrange the variables in proper order and put them to upper case
 #table_F <- table_f_pre2 %>% select(country, year, domain_landings, species, totwghtlandg, no_samples_landg, no_length_measurements_landg, length_unit, min_length, max_length, length, no_length_landg) %>% rename_all(toupper)
-table_F <- table_f_pre2 %>% select(country, year, domain_landings, nep_sub_region, species, totwghtlandg, TOTAL_SAMPLED_TRIPS, no_length_measurements, length_unit, min_length, max_length, length, no_length, mean_weight_at_length, weight_unit) %>% rename_all(toupper)
+table_F <- table_f_pre3 %>% select(country, year, domain_landings, nep_sub_region, species, totwghtlandg, TOTAL_SAMPLED_TRIPS, no_length_measurements, length_unit, min_length, max_length, length, no_length, mean_weight_at_length, weight_unit) %>% rename_all(toupper)
 
 # set working directory to save table F and table of deleted observations
 openxlsx::write.xlsx(table_F,  paste0(path_out,.Platform$file.sep,"TABLE_F_NAO_OFR_LANDINGS_LENGTH.xlsx"), sheetName = "TABLE_F", colNames = TRUE, rowNames = FALSE)
