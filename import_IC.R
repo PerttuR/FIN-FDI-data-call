6c128fb7411ca3a2e67e07616285d0abff537b83
Rantanen Perttu (LUKE) <perttu.rantanen@luke.fi>
2024-06-20 13:44:47 +0300

    SD, SI ja HI rivit 2023 final

--- import_IC.R
+++ import_IC.R
@@ -19,16 +19,25 @@ path_out <- paste0(getwd(), .Platform$file.sep,"results", .Platform$file.sep,"20
 
 
 sd_header <- c("RecordType","Country","Year","SeasonType","Season","Fleet","AreaType","FishingArea","DepthRange","Species","Stock","CatchCategory","ReportingCategory","Sex","CANUMtype","AgeLength","PlusGroup","SampledCatch","NumSamplesLngt","NumLngtMeas","NumSamplesAge","NumAgeMeas","unitMeanWeight","unitCANUM","UnitAgeOrLength","UnitMeanLength","Maturity","NumberCaught","MeanWeight","MeanLength","varNumLanded","varWgtLanded","varLgtLanded")
-# import table A
+# import IC csv data from 2023
 IC_HER_30_2023 <- read.csv2(paste0(path_IC,.Platform$file.sep,"FIN HER 30 2023.csv"), sep = "," , na.strings = "", header = FALSE, col.names=sd_header)
 IC_HER_31_2023 <- read.csv2(paste0(path_IC,.Platform$file.sep,"FIN HER 31 2023.csv"), sep = "," , na.strings = "", header = FALSE, col.names=sd_header)
 IC_HER_32_south_2023 <- read.csv2(paste0(path_IC,.Platform$file.sep,"FIN HER 25-27 28.2 29 and 32 2023.csv"), sep = "," , na.strings = "", header = FALSE, col.names=sd_header)
 IC_SPR_22_32_2023 <- read.csv2(paste0(path_IC,.Platform$file.sep,"FIN SPR 22-32 2023.csv"), sep = "," , na.strings = "", header = FALSE, col.names=sd_header)
 
-# combine by row
-IC_2023a <- rbind(IC_HER_30_2023,IC_HER_31_2023)
-IC_2023b <- rbind(IC_HER_32_south_2023,IC_SPR_22_32_2023)
+# combine rows
+IC_2023 <- rbind(IC_HER_30_2023,IC_HER_31_2023,IC_HER_32_south_2023,IC_SPR_22_32_2023)
 
-IC_2023a2 <- IC_2023a %>% filter(V1 == "SD")
-IC_2023b2 <- IC_2023b %>% filter(V1 == "SD")
+#Filter by SD result rows:
+IC_2023_SD <- IC_2023 %>% filter(RecordType == "SD")
+
+#Filter by SI result rows:
+IC_2023_SI <- IC_2023 %>% filter(RecordType == "SI")
+si_header <- c("RecordType","Country","Year","SeasonType","Season","Fleet","AreaType","FishingArea","DepthRange","Species","Stock","CatchCategory","ReportingCategory","DataToFrom","Usage","SamplesOrigin","QualityFlag","UnitCATON","CATON","OffLandings","varCATON","InfoFleet","InfoStockCoordinator","InfoGeneral")
+colnames(IC_2023_SI) <- si_header 
+
+#Filter by HI result rows:
+IC_2023_HI <- IC_2023 %>% filter(RecordType == "HI")
+hi_header <- c("RecordType","Country","Year","SeasonType","Season","Fleet","AreaType","FishingArea","DepthRange","UnitEffort","Effort","AreaQualifier")
+colnames(IC_2023_HI) <- hi_header 
 
