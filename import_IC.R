f8773764ee9d222ee5e0530878ecafafba41d40c
Rantanen Perttu (LUKE) <perttu.rantanen@luke.fi>
2024-06-25 09:42:42 +0300

    der folder

--- import_IC.R
+++ import_IC.R
@@ -12,18 +12,20 @@ library(dplyr)
 
 
 # Paths & 2024 folder:
-path_IC <- paste0(getwd(), .Platform$file.sep, "orig/IC/")
+path_IC_ORIG <- paste0(getwd(), .Platform$file.sep, "orig/IC/")
+path_der <- paste0(getwd(), .Platform$file.sep, "der/")
 path_rproject <- getwd() # folder where the r project is (and the source file db.R!)
 # folder where the output is saved
 path_out <- paste0(getwd(), .Platform$file.sep,"results", .Platform$file.sep,"2024")
 
 
+
 sd_header <- c("RecordType","Country","Year","SeasonType","Season","Fleet","AreaType","FishingArea","DepthRange","Species","Stock","CatchCategory","ReportingCategory","Sex","CANUMtype","AgeLength","PlusGroup","SampledCatch","NumSamplesLngt","NumLngtMeas","NumSamplesAge","NumAgeMeas","unitMeanWeight","unitCANUM","UnitAgeOrLength","UnitMeanLength","Maturity","NumberCaught","MeanWeight","MeanLength","varNumLanded","varWgtLanded","varLgtLanded")
 # import IC csv data from 2023
-IC_HER_30_2023 <- read.csv2(paste0(path_IC,.Platform$file.sep,"FIN HER 30 2023.csv"), sep = "," , na.strings = "", header = FALSE, col.names=sd_header)
-IC_HER_31_2023 <- read.csv2(paste0(path_IC,.Platform$file.sep,"FIN HER 31 2023.csv"), sep = "," , na.strings = "", header = FALSE, col.names=sd_header)
-IC_HER_32_south_2023 <- read.csv2(paste0(path_IC,.Platform$file.sep,"FIN HER 25-27 28.2 29 and 32 2023.csv"), sep = "," , na.strings = "", header = FALSE, col.names=sd_header)
-IC_SPR_22_32_2023 <- read.csv2(paste0(path_IC,.Platform$file.sep,"FIN SPR 22-32 2023.csv"), sep = "," , na.strings = "", header = FALSE, col.names=sd_header)
+IC_HER_30_2023 <- read.csv2(paste0(path_IC_ORIG,.Platform$file.sep,"FIN HER 30 2023.csv"), sep = "," , na.strings = "", header = FALSE, col.names=sd_header)
+IC_HER_31_2023 <- read.csv2(paste0(path_IC_ORIG,.Platform$file.sep,"FIN HER 31 2023.csv"), sep = "," , na.strings = "", header = FALSE, col.names=sd_header)
+IC_HER_32_south_2023 <- read.csv2(paste0(path_IC_ORIG,.Platform$file.sep,"FIN HER 25-27 28.2 29 and 32 2023.csv"), sep = "," , na.strings = "", header = FALSE, col.names=sd_header)
+IC_SPR_22_32_2023 <- read.csv2(paste0(path_IC_ORIG,.Platform$file.sep,"FIN SPR 22-32 2023.csv"), sep = "," , na.strings = "", header = FALSE, col.names=sd_header)
 
 # combine rows
 IC_2023 <- rbind(IC_HER_30_2023,IC_HER_31_2023,IC_HER_32_south_2023,IC_SPR_22_32_2023)
@@ -50,7 +52,11 @@ IC_2023_HI <- IC_2023_HI %>% select(-RecordType)
 #Left join unitCATOn and CATON values from SI record to SD record
 IC_2023_SD_SI <- left_join(IC_2023_SD, IC_2023_SI)
 #Left join HI to others
-IC_2023_FLAT <- left_join(IC_2023_SD_SI, IC_2023_HI)
+IC_2023_DATA <- left_join(IC_2023_SD_SI, IC_2023_HI)
 
+IC_2023_test <- IC_2023_DATA %>% select(Country,Year,Season,Fleet,FishingArea,Species,AgeLength,SampledCatch, CATON, UnitCATON) 
+
+
+saveRDS(IC_2023_DATA, file = paste0(path_der,.Platform$file.sep,"IC_2023_DATA.rds"))
 
                                       
