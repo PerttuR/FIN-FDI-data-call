767bd82de47f691ecbea63e06af41edba9e5ecd7
Rantanen Perttu (LUKE) <perttu.rantanen@luke.fi>
2024-06-26 12:10:08 +0300

    table f version 1

--- table_f.R
+++ table_f.R
@@ -94,202 +94,46 @@ lengthdata <- read.dbTable("suomu","report_lengthclassrecords")
 #-------------------------------------------------------------------------------
 # choose commercial LANDING samples only, from years 2013-2023 HUOM 2023 valittu kesäkuussa 2024 toimituksessa..
  
-landing <- filter(lengthdata, saalisluokka == "LANDING", projekti == "EU-tike(CS, kaupalliset näytteet)", vuosi >= 2023 & vuosi <= 2023)
+suomu <- filter(lengthdata, projekti == "EU-tike(CS, kaupalliset näytteet)", vuosi >= 2023 & vuosi <= 2023,fao %in% c("HER", "SPR"))
+
+suomu$metier <- suomu$metiers_fk
 
 #-------------------------------------------------------------------------------
 
 # make a key variable to match table A key (domain_discards or domain_landings)
 
 # first make individually all the parts that form the key
-country_code <- "FIN"
-quarter <- landing$q
-subregion <- paste("27.3.D.", landing$ices_osa_alue, sep = "")
-#Stat dep uses FPO instead of FPN so change
-landing <- landing %>% mutate(metiers_fk = replace(metiers_fk,metiers_fk=="FPN_FWS_>0_0_0","FPO_FWS_>0_0_0"))
-landing <- landing %>% mutate(metiers_fk = replace(metiers_fk,metiers_fk=="FPN_SPF_>0_0_0","FPO_SPF_>0_0_0"))
-gear_type <- landing$metiers_fk
-unique(gear_type)
-
-gear_type <- landing$metiers_fk
-
-# codes for vessel length from appendix 2:
-landing$vessel_length_code[landing$laivan_pituus_cm < 1000] <- "VL0010"
-landing$vessel_length_code[landing$laivan_pituus_cm >= 1000 & landing$laivan_pituus_cm < 1200] <- "VL1012"
-landing$vessel_length_code[landing$laivan_pituus_cm >= 1200 & landing$laivan_pituus_cm < 1800] <- "VL1218"
-landing$vessel_length_code[landing$laivan_pituus_cm >= 1800 & landing$laivan_pituus_cm < 2400] <- "VL1824"
-landing$vessel_length_code[landing$laivan_pituus_cm >= 2400 & landing$laivan_pituus_cm < 4000] <- "VL2440"
-landing$vessel_length_code[landing$laivan_pituus_cm >= 4000] <- "VL40XX"
-landing$vessel_length_code[is.na(landing$laivan_pituus_cm)] <- "NK"
-
-vessel_length <- landing$vessel_length_code
-
-species <- landing$fao
-commercial_cat <- "NA"
-
-# then combine them as a single key, identical to that from table A
-landing$domain_landings <- paste(country_code, quarter, subregion, gear_type, vessel_length, species, commercial_cat, sep = "_")
-
-landing2 <- landing %>% select(vuosi, domain_landings, nayteno, pituusluokka, pituusluokan_kpl_maara, pituusluokan_kokpaino)
-
-#--------------------------------------------------------------------------------------------
-#       3. aggregate SALMON data to length classes and merge it with LANDING data                       
-#--------------------------------------------------------------------------------------------
-
-# download anadromous species sampling data from : http://suomu.rktl.fi/lohi/Report/stecf?format=csv
-# and save it to workflow orig folder
-# import data from salmon samples
-
-ana1 <- read.csv(paste0(path_salmon, "anadromous_samples_2013_2021.csv"), sep = ";", header = T, stringsAsFactors=FALSE)
-
-#--------------------------------------------------------------------------------------------
-#       3.1 Functions to use Mongo Individual data                      
-#--------------------------------------------------------------------------------------------
-
-
-
-get.rectangle <- function(individual) {
-  rectangle <- ices_rectangle[ices_rectangle$rktl_name ==first(individual$ruutu),]
-  if(nrow(rectangle) != 1) {
-    stop("Missing rectangle")
-  }
-  return(rectangle)
-}
-
-get.stock <- function(batch, rectangle) {
-  if(batch$aphia_id == 127186) {
-    if(rectangle$rdb_area_code == "27.3.d.32") {
-      return("sal-32")
-    } else {
-      return("sal-2431")
-    }
-  }
-  return("")
-}
-
-get.age <- function(individual) {
-  mevu <- as.integer(individual$me_vu)
-  povu <- as.integer(individual$po_vu)
-  povu <- ifelse(is.na(povu), 2, povu)
-  return(ifelse(is.na(mevu), NA, mevu + povu))
-}
-
-
-#--------------------------------------------------------------------------------------------
-#       3.2 Merge ANA1 (Oracle ana-samples 2013-2021) and ANA2 (MOngo 2022 onwards anadromous samples)                    
-#--------------------------------------------------------------------------------------------
-
-source("mongo.R")
-
-ices_rectangle <- read.csv2("ices_rectangle.csv", sep=",", dec=".")
-
-batch <- read.mongoCollectionToDataframe("batch")
-individual <- read.mongoCollectionToDataframe("individual")
-species <- read.mongoCollectionToDataframe("species")
-species$laji <- as.character(species$laji)
-batch <- batch %>% left_join(species, by = c("species_id" = "laji"))
-project <- read.mongoCollectionToDataframe("project")
-
-# filter only landed salmon and sea trout samples
-individual$pitcm <-  as.integer(individual$pitcm)
-# select landing salmons and sea trouts based on length
-individual <- individual  %>% filter(pitcm >= 60) 
-
-
-# Resolve trip names
-
-batch$trip_id <- paste(batch$year, batch$lajikv1, batch$batch_number, sep="_")
-
-#join batch to individual
-
-individual2 <- individual %>% left_join(batch, by = c("sample_id" = "batch_id"))
-
-#join project to individual
-
-individual3 <- individual2 %>% left_join(project, by = c("project_id" = "project_id"))
-
-#filter empty lenths
-#filter only commercial samples
-#form lengthclasses
-individual4 <- individual3 %>%
-  filter(!is.na(as.numeric(pitcm))) %>%
-  filter(number==0) %>% #filter only commercial samples included 
-  mutate(length = as.numeric(pitcm) * 10) %>%
-  mutate(lengthclass = length - length %% 10) %>%
-  arrange(lengthclass, length)
-
-#renaming
-individual4$PITUUS <- individual4$length
-individual4$DB_TRIP_ID <- individual4$trip_id
-individual4$YEAR <- individual4$year
-individual4$PYYDYSKOODI <- individual4$pyydys
-
-#Age to a single number from me_vu and po_vu variables
-individual4$IKA <- get.age(individual4)
-
-#weight in grams
-individual4$PAINO_GRAMMOINA <- individual4$pakg * 1000
-
-#calculate month
-individual4$MONTH <- format(as.Date(individual4$pvm, format="%Y-%m-%d"),"%m")
-
-#add year Quarters
-Q1 <- c("01","02","03")
-Q2 <- c("04","05","06")
-Q3 <- c("07","08","09")
-Q4 <- c("10","11","12")
-individual4$QUARTER [individual4$MONTH %in% Q1]<-1
-individual4$QUARTER [individual4$MONTH %in% Q3]<-3
-individual4$QUARTER [individual4$MONTH %in% Q4]<-4
-individual4$QUARTER [individual4$MONTH %in% Q2]<-2
-
-individual4$MONTH <- as.numeric(individual4$MONTH)
-individual4$PYYDYSKOODI <- as.numeric(individual4$PYYDYSKOODI)
-
-#rename SD, add metier, rename species code
-individual4$ICES_OA <- as.numeric(individual4$osa_al)
-individual4$METIER <- "FYK_ANA_>0_0_0"
-individual4$FAO <- individual4$lajikv1
-
-# TO DO select correct variables from MONGO data as ana2
-
-ana2 <- individual4
-#cast Oracle DB_TRIP_ID to character
-ana1$DB_TRIP_ID <- as.character(ana1$DB_TRIP_ID)
-
-ana <- dplyr::bind_rows(ana1, ana2)
-
-#rename metier to correct
-#DEL: salmon <- salmon %>% mutate(METIER=replace(METIER, METIER=="FYK_ANA_0_0_0", "FYK_ANA_>0_0_0")) %>% as.data.frame()
-
-#2021 data call filter 2014 and 2020
-#DEL: ana <- ana %>% filter( YEAR == 2014 | YEAR == 2020)
-
 # make a key variable to match table A key (domain_discards or domain_landings)
 
 # first make individually all the parts that form the key
 country_code <- "FIN"
-quarter <- ana$QUARTER
-subregion <- paste("27.3.D.", ana$ICES_OA, sep = "")
-gear_type <- ana$METIER
-vessel_length <- "VL0010"
-species <- ana$FAO
-commercial_cat <- "NA"
+quarter <- suomu$q
+subregion <- paste("27.3.d.", suomu$ices_osa_alue, sep = "")
+#Stat dep uses FPO instead of FPN so change
+suomu <- suomu %>% mutate(metier = replace(metier,metier=="FPN_FWS_>0_0_0","FPO_FWS_>0_0_0"))
+suomu <- suomu %>% mutate(metier = replace(metier,metier=="FPN_SPF_>0_0_0","FPO_SPF_>0_0_0"))
+gear_type <- suomu$fishframe
+gear_type <- case_when(gear_type == "FPO" | gear_type == "FPN" | gear_type == "FYK" ~ "FPO-FPN-FYK",
+                       gear_type == "OTM" | gear_type == "PTM" ~ "OTM-PTM",
+                       TRUE ~ gear_type
+)
+
+unique(gear_type)
+
+TARGET_ASSEMBLAGE <- substr(suomu$metier,5,7)#metieristä merkit 5-7
+mesh_size <- "all"
+selective_device <- "NA"
+mesh_size_range_selective_device <- "NA"
+vessel_length <- "all"
+species <- suomu$fao
+commercial_cat <- "all"
 
 # then combine them as a single key, identical to that from table A
-ana$domain_landings <- paste(country_code, quarter, subregion, gear_type, vessel_length, species, commercial_cat, sep = "_")
+suomu$domain_landings <- paste(country_code, quarter, subregion, gear_type, TARGET_ASSEMBLAGE, mesh_size, selective_device, mesh_size_range_selective_device, vessel_length, species, commercial_cat, sep = "_")
 
-## aggregate data into length classes (assuming there are no fish under 300 or over 1250)
-pit_bins <- seq(300,1250, by=50)
-ana$pituusluokka <- pit_bins[findInterval(ana$PITUUS, pit_bins)]
+suomu$species <- suomu$fao
 
-# aggregate data to the same level as landings data (by length CLASS)
-ana_length <- ana %>% group_by(YEAR, domain_landings, DB_TRIP_ID, pituusluokka) %>% summarise(pituusluokan_kpl_maara = n(), pituusluokan_kokpaino = sum(PAINO_GRAMMOINA)) %>% rename(nayteno = DB_TRIP_ID, vuosi = YEAR)
-
-
-# merge landing and salmon data
-
-landing3 <- merge(landing2, ana_length, all = T)
-
+suomu2 <- suomu %>% select(vuosi, domain_landings, species, nayteno, pituusluokka, pituusluokan_kpl_maara, pituusluokan_kokpaino)
 
 
 #-----------------------------------------------------------------------------------
@@ -299,22 +143,22 @@ landing3 <- merge(landing2, ana_length, all = T)
 # aggregate data on different levels according to Annex D instructions from the Official Letter
 
 #number of samples (number of TRIPS) + sum class bumber + mean weight at length
-d6_7 <- landing3 %>% group_by(vuosi, domain_landings) %>% summarise(TOTAL_SAMPLED_TRIPS = n_distinct(nayteno), no_length_measurements = sum(pituusluokan_kpl_maara)) 
+d6_7 <- suomu2 %>% group_by(vuosi, domain_landings, species) %>% summarise(TOTAL_SAMPLED_TRIPS = n_distinct(nayteno), no_length_measurements = sum(pituusluokan_kpl_maara)) 
 
 # minimum and maximum lengths (notice! this is done by trip as well)
-d9_10 <- landing3 %>% group_by(vuosi, domain_landings) %>% summarise(min_length = sum(min(pituusluokka)), max_length = sum(max(pituusluokka)))
+d9_10 <- suomu2 %>% group_by(vuosi, domain_landings, species) %>% summarise(min_length = sum(min(pituusluokka)), max_length = sum(max(pituusluokka)))
 
 #number of length measurements 
-d11_12 <- landing3 %>% group_by(vuosi, domain_landings, pituusluokka) %>% summarise(no_length = sum(pituusluokan_kpl_maara), mean_weight_at_length = mean(pituusluokan_kokpaino/pituusluokan_kpl_maara, na.rm = TRUE))
+d11_12 <- suomu2 %>% group_by(vuosi, domain_landings,species, pituusluokka) %>% summarise(no_length = sum(pituusluokan_kpl_maara), mean_weight_at_length = mean(pituusluokan_kokpaino/pituusluokan_kpl_maara, na.rm = TRUE))
 d11_12$mean_weight_at_length <- round(d11_12$mean_weight_at_length, digits = 0)
 
 
 
 # merge the aggregated datas (above) to landing catch data 
 
-landing4 <- merge(d11_12, d9_10, by = c("vuosi", "domain_landings"))
+landing4 <- merge(d11_12, d9_10, by = c("vuosi", "domain_landings","species"))
 
-landing5 <- merge(landing4, d6_7, by = c("vuosi", "domain_landings"))
+landing5 <- merge(landing4, d6_7, by = c("vuosi", "domain_landings","species"))
 
 # add length_unit and country variables
 landing5$length_unit <- "mm"
