d0c7f28d7976f6f140ae981862037945d13e9e1a
Joanne Demmler <j.demmler@gmx.com>
2025-05-19 10:37:33 +0300

    adding libraries

--- quick_map_jcd.R
+++ quick_map_jcd.R
@@ -1,3 +1,8 @@
+library(tidyverse)      # dplyr, ggplot etc.
+library(sf)             # simple spatial features
+library(readxl)
+
+
 # grab map data for Baltic states
 p <- c("Finland","Sweden","Norway","Russia","Denmark","Germany",
        "Estonia","Latvia","Lithuania","Belarus","Poland")
