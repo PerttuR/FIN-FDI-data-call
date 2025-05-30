9cbed5012b5aa8fd3e1567d5e5070807c465e7bc
Joanne Demmler <j.demmler@gmx.com>
2025-05-30 10:06:47 +0300

    new script for 2025 issues update issues pptx

---
+++ issues_2025.R
@@ -0,0 +1,37 @@
+# ISSUE 1 ####
+
+library(tidyverse)
+library(flextable)
+library(janitor)
+
+G_table <- read.csv("C:/Users/03269737/OneDrive - Valtion/Projects/FIN-FDI-data-call/orig/G_table_2013_2022_SAS.csv")
+I_table <- read.csv("C:/Users/03269737/OneDrive - Valtion/Projects/FIN-FDI-data-call/orig/I_table_2013_2022_SAS.csv")
+
+
+sum.G <- G_table |> filter(YEAR == 2013) |> 
+  group_by(METIER) |> 
+  summarise(TOTFISHDAYS.G = sum(TOTFISHDAYS, na.rm=TRUE)) |> 
+  adorn_totals("row")
+
+sum.G |>
+  flextable() |> 
+  set_caption("table_G")
+
+sum.I <- I_table |> filter(YEAR == 2013) |> 
+  group_by(METIER) |> 
+  summarise(TOTFISHDAYS.I = sum(TOTFISHDAYS, na.rm=TRUE)) |> 
+  adorn_totals("row")
+
+sum.I |>
+  flextable() |> 
+  set_caption("table_I")
+
+
+join.IG <- sum.G |> full_join(sum.I) |> 
+            mutate(DIFF = abs(TOTFISHDAYS.G-TOTFISHDAYS.I),
+                                               DIFF_PCT = round(DIFF/if_else(TOTFISHDAYS.G>TOTFISHDAYS.I, TOTFISHDAYS.G, TOTFISHDAYS.I)*100,3))
+
+join.IG |> flextable() |> set_caption("Difference I/G")
+
+
+# ISSUE 2 ####
