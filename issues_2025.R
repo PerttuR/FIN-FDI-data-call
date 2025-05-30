ed9297f3e0406360bd15ed44c823d3454c3bffca
Joanne Demmler <j.demmler@gmx.com>
2025-05-30 10:32:31 +0300

    adding syntax highlightin and devider line to issue 1 comparison table

--- issues_2025.R
+++ issues_2025.R
@@ -3,6 +3,7 @@
 library(tidyverse)
 library(flextable)
 library(janitor)
+library(officer)
 
 G_table <- read.csv("C:/Users/03269737/OneDrive - Valtion/Projects/FIN-FDI-data-call/orig/G_table_2013_2022_SAS.csv")
 I_table <- read.csv("C:/Users/03269737/OneDrive - Valtion/Projects/FIN-FDI-data-call/orig/I_table_2013_2022_SAS.csv")
@@ -15,7 +16,9 @@ sum.G <- G_table |> filter(YEAR == 2013) |>
 
 sum.G |>
   flextable() |> 
-  set_caption("table_G")
+  set_caption("table_G") |> 
+  hline(i=dim(sum.G)[1]-1, border=fp_border(width=1.5))
+  
 
 sum.I <- I_table |> filter(YEAR == 2013) |> 
   group_by(METIER) |> 
@@ -24,14 +27,17 @@ sum.I <- I_table |> filter(YEAR == 2013) |>
 
 sum.I |>
   flextable() |> 
-  set_caption("table_I")
+  set_caption("table_I") |> 
+  hline(i=dim(sum.G)[1]-1, border=fp_border(width=1.5))
 
 
 join.IG <- sum.G |> full_join(sum.I) |> 
             mutate(DIFF = abs(TOTFISHDAYS.G-TOTFISHDAYS.I),
                                                DIFF_PCT = round(DIFF/if_else(TOTFISHDAYS.G>TOTFISHDAYS.I, TOTFISHDAYS.G, TOTFISHDAYS.I)*100,3))
 
-join.IG |> flextable() |> set_caption("Difference I/G")
+join.IG |> flextable() |> set_caption("Difference I/G") |>
+  highlight(i=which(join.IG$DIFF_PCT >2), color="yellow") |>
+  hline(i=dim(join.IG)[1]-1, border=fp_border(width=1.5))
 
 
 # ISSUE 2 ####
