c205698bf4dd358baf757b498d1238caeaada2b2
Joanne Demmler <j.demmler@gmx.com>
2025-09-03 09:13:47 +0300

    split number of vessels by year and do tables in loop

--- sdc_herring_tables.R
+++ sdc_herring_tables.R
@@ -16,32 +16,62 @@ capacity |> count(Country) |> print(n=Inf)
 baltic <- c("Denmark", "Estonia", "Finland", "Germany", "Latvia", "Lithuania", "Sweden")
 
 capacity <- capacity |> filter(Country %in% baltic)
+years <- sort(unique(capacity$Year))
 
 # number of vessels by vessel length category for each Baltic country ####
-table_nums("num_vessels", "Number of vessels by vessel length category for each Baltic country.")
 
-ftab <- capacity |> filter(`Vessel Length Category` != "NK") |>
-  select(Country, `Vessel Length Category`, `Total vessels` ) |>
-  group_by(Country, `Vessel Length Category`) |> 
+by <- capacity |> filter(`Vessel Length Category` != "NK") |>
+  select(Country, Year, `Vessel Length Category`, `Total vessels` ) |>
+  group_by(Country, Year, `Vessel Length Category`) |> 
   summarise(sum.vessels = sum(`Total vessels`, na.rm=TRUE)) |>
-  pivot_wider(names_from = `Vessel Length Category`, values_from = sum.vessels) |> 
+  pivot_wider(names_from = `Vessel Length Category`, values_from = sum.vessels) |>
+  ungroup() |> group_split(Year)
+
+
+for (i in 1:length(by)){
+
+table_nums(paste0("num_vessels_",i), 
+           paste("Number of vessels by vessel length category for each Baltic country in", years[i], "."))
+  
+ftab <- by[[i]] |> select(-Year) |>
   flextable() |> autofit() |>
   set_caption(as_paragraph(
-    as_chunk(table_nums("num_vessels"), props = fp_text_default(font.family = "Arial", bold=TRUE))
+    as_chunk(table_nums(paste0("num_vessels_",i)), props = fp_text_default(font.family = "Arial", bold=TRUE))
   ), word_stylename = "Table Caption")
-    
-print(ftab, preview = "docx")   
+
+print(ftab)   
+# print(ftab, preview = "docx")   
+
+}
+
 
 # avg age of vessels by vessel length category for each Baltic country ####
-table_nums("age_vessels", "Average age of vessels by vessel length category for each Baltic country.")
 
-capacity |> filter(`Vessel Length Category` != "NK") |>
-  select(Country, `Vessel Length Category`, `Average age` ) |>
-  group_by(Country, `Vessel Length Category`) |> 
+by <- capacity |> filter(`Vessel Length Category` != "NK") |>
+  select(Country, Year, `Vessel Length Category`, `Average age`) |>
+  group_by(Country, Year, `Vessel Length Category`) |> 
   summarise(mean.age = mean(`Average age`, na.rm=TRUE)) |>
-  pivot_wider(names_from = `Vessel Length Category`, values_from = mean.age) |> 
-  flextable() |> 
-  set_caption(table_nums("age_vessels"))
+  pivot_wider(names_from = `Vessel Length Category`, values_from = mean.age) |>
+  ungroup() |> group_split(Year)
+
+
+for (i in 1:length(by)){
+  
+  table_nums(paste0("age_vessels_",i), 
+             paste("Average age of vessels by vessel length category for each Baltic country in", years[i], "."))
+  
+  ftab <- by[[i]] |> select(-Year) |>
+    flextable() |> autofit() |>
+    set_caption(as_paragraph(
+      as_chunk(table_nums(paste0("age_vessels_",i)), props = fp_text_default(font.family = "Arial", bold=TRUE))
+    ), word_stylename = "Table Caption")
+  
+  print(ftab)   
+  # print(ftab, preview = "docx")   
+  
+}
+
+
 
 # avg length of vessels by vessel length category for each Baltic country ####
 table_nums("length_vessels", "Average length of vessels by vessel length category for each Baltic country.")
